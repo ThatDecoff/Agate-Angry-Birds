c5f3d4be235109010164a8690afe0ab0dd9b874e
Adrian Kaiser <adriankaiser.rian@gmail.com>
2021-10-04 09:23:38 +0700

    added new bird owl

---
+++ Angry Birds/Assets/Scripts/Explosion.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion : MonoBehaviour
+{
+    //public PointEffector2D effector;
+    public float timer = 0.2f;
+
+    private float _timer;
+
+    private void Start()
+    {
+        _timer = timer;
+    }
+
+    private void Update()
+    {
+        _timer -= Time.deltaTime;
+        if(_timer <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Angry Birds/Assets/Scripts/OwlBird.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OwlBird : Bird
+{
+    public Explosion explosionPrefab;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Instantiate(explosionPrefab, transform.position, Quaternion.identity);
+    }
+}
